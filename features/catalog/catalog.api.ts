4773df4866c505214c6b33fc94b66829c4705656
shubhsingh1515 <shubhsingh1515@gmail.com>
2026-09-01 14:40:11 +0530

    added category api on service catalog page

--- features/catalog/catalog.api.ts
+++ features/catalog/catalog.api.ts
@@ -16,3 +16,8 @@ export const getAllSearchedActiveItems = async (search: any) => {
   console.log("this is the response===>>", res);
   return res;
 };
+
+export const getCatalogCategoriesApi = async () => {
+  const res = await oldApiClient.get(`/v1/catalog?isActive=true&limit=100`);
+  return res;
+};
